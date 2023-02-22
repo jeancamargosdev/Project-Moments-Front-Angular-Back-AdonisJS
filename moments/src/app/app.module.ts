f2b52c43d9d4e84e174d7cf904b4e125c1cd8b80
jmcamargos <jeanmatheus@hotmail.com>
2023-02-22 18:49:42 -0300

    inicializating message component

--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -22,10 +24,11 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     AboutComponent,
     HomeComponent,
     NewMomentComponent,
-    MomentFormComponent
+    MomentFormComponent,
+    MessagesComponent
   ],
   imports: [
-    BrowserModule,   AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule,
+    BrowserModule,   AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule, FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
