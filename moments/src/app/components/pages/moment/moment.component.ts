da5aa21e5c3a9e191766b9af1756ee5186442661
jmcamargos <jeanmatheus@hotmail.com>
2023-02-26 10:09:04 -0300

    inserindo comentários

--- moments/src/app/components/pages/moment/moment.component.ts
+++ moments/src/app/components/pages/moment/moment.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormGroupDirective  } from '@angular/forms';
 
 import { MomentService } from 'src/app/services/moment.service';
 
 import {Moment} from 'src/app/Moment';
+import { Comment } from 'src/app/Comment';
+import { CommentService } from 'src/app/services/comment.service';
 
 import { environment } from 'src/environments/environments';
 
@@ -23,11 +26,14 @@ export class MomentComponent implements OnInit{
   faTimes = faTimes;
   faEdit = faEdit;
 
+  commentForm! : FormGroup;
+
   constructor(
     private momentService: MomentService,
     private route: ActivatedRoute,
     private messagesService:MessagesService,
-    private router: Router
+    private router: Router,
+    private commentService: CommentService
     ){}
 
   ngOnInit(): void{
@@ -37,6 +43,19 @@ export class MomentComponent implements OnInit{
     .getMoment(id)
     .subscribe((item) => (this.moment = item.data));
 
+    this.commentForm = new FormGroup({
+      text: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required]),
+    });
+
+  }
+
+  get text() {
+    return this.commentForm.get('text')!;
+  }
+
+  get username(){
+    return this.commentForm.get('username')!;
   }
 
   async removeHandler(id: number){
@@ -45,5 +64,21 @@ export class MomentComponent implements OnInit{
     this.router.navigate(['/']);
   }
 
+ async onSubmit(formDirective: FormGroupDirective){
+    if(this.commentForm.invalid){
+      return
+    }
+
+    const data: Comment = this.commentForm.value;
+    data.momentId = Number(this.moment!.id);
+
+    await this.commentService.createComment(data).subscribe((comment) => this.moment!.comments!.push(comment.data));
+
+    this.messagesService.add("Comentário adicionado!");
+    this.commentForm.reset();
+
+    formDirective.resetForm();
+
+  }
 
 }
